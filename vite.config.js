50f458bfed17a7b79f1cfe4bb043029466760bd4
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2025-11-24 21:30:19 +0530

    fix error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/react-weather-app/', // 👈 Add this line
   plugins: [
     react(),       // 👈 adds React JSX transform support
     tailwindcss(), // 👈 keeps Tailwind working
